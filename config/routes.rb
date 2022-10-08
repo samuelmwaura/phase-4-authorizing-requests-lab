885a4775aeaa174a3f65555d78229edb406e9a48
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-08 12:50:33 +0300

    commit tree f2c3295d5aaf7e68bfe405cf35bbd803a5f92f4d

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show]
-  
+  resources :articles, only: [:index, :show]  
   resources :members_only_articles, only: [:index, :show]
 
+  #Routes for the login functionality - create a user session, fetch a logged in user session, delete a users session to log them out.
   get "/me", to: "users#show"
-
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 end
