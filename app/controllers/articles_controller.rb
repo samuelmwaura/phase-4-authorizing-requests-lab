7a6ca2b0a7804f893c940aacb34b933bd579348d
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-08 12:50:52 +0300

    Completed the lab

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  #GET /articles
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
+  #GET /articles/:id
   def show
     article = Article.find(params[:id])
     render json: article
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
   
+  #POST /login
   def create
     user = User.find_by(username: params[:username])
     session[:user_id] = user.id
     render json: user
   end
 
+  #DELETE /logout
   def destroy
     session.delete :user_id
     head :no_content
