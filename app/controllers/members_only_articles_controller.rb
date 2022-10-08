d55909f6647c5518ae6fc96386e81f41b884e0b7
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-08 13:04:25 +0300

    Adding some clarity

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,6 +1,9 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   before_action :authorize
+  #This means that before anny handle function in this controller, the aurthorize method is going to run amd check wheher thr requester client is logged in.
+  #Is it is needed that any of the contollers skips the authentication, the skip_before_action method is used and the exact method to skip is indicate explicitly.
+  #This is a way to authorize and to expose some resources to non-logged-in users and other specific ones to logged in users.
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
